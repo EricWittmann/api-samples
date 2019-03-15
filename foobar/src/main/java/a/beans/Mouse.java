c2c8e7541955a7531613da124495f9e8ef969947
Eric Wittmann <eric.wittmann@gmail.com>
2019-03-15 10:36:27 -0400

    asfdsdaf

---
+++ foobar/src/main/java/a/beans/Mouse.java
@@ -0,0 +1,47 @@
+
+package a.beans;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+/**
+ * Root Type for Mouse
+ * <p>
+ * The root of the Mouse type's schema.
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "name",
+    "size"
+})
+public class Mouse {
+
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("size")
+    private Integer size;
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("size")
+    public Integer getSize() {
+        return size;
+    }
+
+    @JsonProperty("size")
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+}
