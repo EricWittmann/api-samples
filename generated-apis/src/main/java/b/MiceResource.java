e69c56c0f220e46c3840729b47082be0f401b3c4
Eric Wittmann <eric.wittmann@gmail.com>
2019-03-15 10:30:47 -0400

    asdfasdf

---
+++ generated-apis/src/main/java/b/MiceResource.java
@@ -0,0 +1,56 @@
+package b;
+
+import b.beans.Mouse;
+import java.lang.String;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+/**
+ * A JAX-RS interface.  An implementation of this interface must be provided.
+ */
+@Path("/mice")
+public interface MiceResource {
+  /**
+   * Gets a list of all `Mouse` entities.
+   */
+  @GET
+  @Produces("application/json")
+  List<Mouse> getMice();
+
+  /**
+   * Creates a new instance of a `Mouse`.
+   */
+  @POST
+  @Consumes("application/json")
+  void createMouse(Mouse data);
+
+  /**
+   * Gets the details of a single instance of a `Mouse`.
+   */
+  @Path("/{mouseId}")
+  @GET
+  @Produces("application/json")
+  Mouse getMouse(@PathParam("mouseId") String mouseId);
+
+  /**
+   * Updates an existing `Mouse`.
+   */
+  @Path("/{mouseId}")
+  @PUT
+  @Consumes("application/json")
+  void updateMouse(@PathParam("mouseId") String mouseId, Mouse data);
+
+  /**
+   * Deletes an existing `Mouse`.
+   */
+  @Path("/{mouseId}")
+  @DELETE
+  void deleteMouse(@PathParam("mouseId") String mouseId);
+}
