53e98632de033165e2e473a651acd08618084e03
Eric Wittmann <eric.wittmann@gmail.com>
2019-10-11 10:25:58 -0400

    This is a demo.

--- quarkusapps/src/main/java/io/example/beer/BeersResource.java
+++ quarkusapps/src/main/java/io/example/beer/BeersResource.java
@@ -3,12 +3,14 @@ package io.example.beer;
 import io.example.beer.beans.Beer;
 import java.lang.String;
 import java.util.List;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 /**
  * A JAX-RS interface.  An implementation of this interface must be provided.
@@ -19,12 +21,16 @@ public interface BeersResource {
    * Gets a list of all `Beer` entities.
    */
   @GET
+  @Produces("application/json")
+  @Consumes("application/json")
   List<Beer> getbeers();
 
   /**
    * Creates a new instance of a `Beer`.
    */
   @POST
+  @Produces("application/json")
+  @Consumes("application/json")
   void createBeer(Beer body);
 
   /**
@@ -32,6 +38,8 @@ public interface BeersResource {
    */
   @Path("/{beerId}")
   @GET
+  @Produces("application/json")
+  @Consumes("application/json")
   Beer getBeer(@PathParam("beerId") String beerId);
 
   /**
@@ -39,6 +47,8 @@ public interface BeersResource {
    */
   @Path("/{beerId}")
   @PUT
+  @Produces("application/json")
+  @Consumes("application/json")
   void updateBeer(@PathParam("beerId") String beerId, Beer body);
 
   /**
@@ -46,5 +56,7 @@ public interface BeersResource {
    */
   @Path("/{beerId}")
   @DELETE
+  @Produces("application/json")
+  @Consumes("application/json")
   void deleteBeer(@PathParam("beerId") String beerId);
 }
