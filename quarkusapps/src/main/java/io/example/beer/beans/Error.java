aae324242a38ce8532b99215e58847b7f0643950
Eric Wittmann <eric.wittmann@gmail.com>
2019-10-11 10:19:10 -0400

    This is a demo.

---
+++ quarkusapps/src/main/java/io/example/beer/beans/Error.java
@@ -0,0 +1,19 @@
+
+package io.example.beer.beans;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+/**
+ * 
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+
+})
+public class Error {
+
+
+}
