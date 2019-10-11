e1cf5d1441df7cb6a25e517c023da70fd447e939
Eric Wittmann <eric.wittmann@gmail.com>
2019-10-10 21:27:55 -0400

    qa

---
+++ qa/src/main/java/abc/WidgetsResource.java
@@ -0,0 +1,56 @@
+package abc;
+
+import abc.beans.Widget;
+import java.lang.String;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+/**
+ * A JAX-RS interface.  An implementation of this interface must be provided.
+ */
+@Path("/widgets")
+public interface WidgetsResource {
+  /**
+   * Gets a list of all `Widget` entities.
+   */
+  @GET
+  @Produces("application/json")
+  List<Widget> getwidgets();
+
+  /**
+   * Creates a new instance of a `Widget`.
+   */
+  @POST
+  @Consumes("application/json")
+  void createWidget(Widget data);
+
+  /**
+   * Gets the details of a single instance of a `Widget`.
+   */
+  @Path("/{widgetId}")
+  @GET
+  @Produces("application/json")
+  Widget getWidget(@PathParam("widgetId") String widgetId);
+
+  /**
+   * Updates an existing `Widget`.
+   */
+  @Path("/{widgetId}")
+  @PUT
+  @Consumes("application/json")
+  void updateWidget(@PathParam("widgetId") String widgetId, Widget data);
+
+  /**
+   * Deletes an existing `Widget`.
+   */
+  @Path("/{widgetId}")
+  @DELETE
+  void deleteWidget(@PathParam("widgetId") String widgetId);
+}
