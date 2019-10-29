a1fbcd087d8cf7fa5d59892449e8d2f0fead0b1e
Eric Wittmann <eric.wittmann@gmail.com>
2019-10-29 08:52:11 -0400

    asdfasdf

---
+++ demo/widget/src/main/java/c/beans/Widget.java
@@ -0,0 +1,74 @@
+
+package c.beans;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+/**
+ * Root Type for Widget
+ * <p>
+ * What is a widget!!
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "name",
+    "size",
+    "description"
+})
+public class Widget {
+
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("size")
+    private Integer size;
+    /**
+     * 
+     * 
+     */
+    @JsonProperty("description")
+    @JsonPropertyDescription("")
+    private String description;
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("size")
+    public Integer getSize() {
+        return size;
+    }
+
+    @JsonProperty("size")
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    /**
+     * 
+     * 
+     */
+    @JsonProperty("description")
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * 
+     * 
+     */
+    @JsonProperty("description")
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
