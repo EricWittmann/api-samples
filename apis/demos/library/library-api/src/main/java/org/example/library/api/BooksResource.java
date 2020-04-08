f3c5ab581228124bc9012594c7f98fa5fc4c69a2
Eric Wittmann <eric.wittmann@gmail.com>
2020-04-08 14:13:49 -0400

    Bootstrap new Library API impl.

---
+++ apis/demos/library/library-api/src/main/java/org/example/library/api/BooksResource.java
@@ -0,0 +1,56 @@
+package org.example.library.api;
+
+import java.lang.String;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import org.example.library.api.beans.Book;
+
+/**
+ * A JAX-RS interface.  An implementation of this interface must be provided.
+ */
+@Path("/books")
+public interface BooksResource {
+  /**
+   * Gets a list of all `Book` entities.
+   */
+  @GET
+  @Produces("application/json")
+  List<Book> getbooks();
+
+  /**
+   * Creates a new instance of a `Book`.
+   */
+  @POST
+  @Consumes("application/json")
+  void createBook(Book data);
+
+  /**
+   * Gets the details of a single instance of a `Book`.
+   */
+  @Path("/{bookId}")
+  @GET
+  @Produces("application/json")
+  Book getBook(@PathParam("bookId") String bookId);
+
+  /**
+   * Updates an existing `Book`.
+   */
+  @Path("/{bookId}")
+  @PUT
+  @Consumes("application/json")
+  void updateBook(@PathParam("bookId") String bookId, Book data);
+
+  /**
+   * Deletes an existing `Book`.
+   */
+  @Path("/{bookId}")
+  @DELETE
+  void deleteBook(@PathParam("bookId") String bookId);
+}
---
+++ apis/demos/library/library-api/src/main/java/org/example/library/api/beans/Book.java
@@ -0,0 +1,93 @@
+
+package org.example.library.api.beans;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+/**
+ * Root Type for Book
+ * <p>
+ * Information about a book.
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "ddsn",
+    "title",
+    "author",
+    "publish-date"
+})
+public class Book {
+
+    @JsonProperty("ddsn")
+    private String ddsn;
+    @JsonProperty("title")
+    private String title;
+    /**
+     * Root Type for Author
+     * <p>
+     * The author of a book.
+     * 
+     */
+    @JsonProperty("author")
+    @JsonPropertyDescription("The author of a book.")
+    private Author author;
+    @JsonProperty("publish-date")
+    private String publishDate;
+
+    @JsonProperty("ddsn")
+    public String getDdsn() {
+        return ddsn;
+    }
+
+    @JsonProperty("ddsn")
+    public void setDdsn(String ddsn) {
+        this.ddsn = ddsn;
+    }
+
+    @JsonProperty("title")
+    public String getTitle() {
+        return title;
+    }
+
+    @JsonProperty("title")
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Root Type for Author
+     * <p>
+     * The author of a book.
+     * 
+     */
+    @JsonProperty("author")
+    public Author getAuthor() {
+        return author;
+    }
+
+    /**
+     * Root Type for Author
+     * <p>
+     * The author of a book.
+     * 
+     */
+    @JsonProperty("author")
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @JsonProperty("publish-date")
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    @JsonProperty("publish-date")
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
+
+}
